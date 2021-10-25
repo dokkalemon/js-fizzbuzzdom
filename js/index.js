648cd936e44672f2c8cd882c52171df67a036d3c
dokkalemon <dottori.roberto@gmail..com>
2021-10-25 16:34:45 +0200

    add the box

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.box-container');
 
 
 
 
-for (let row = 0; row <= 15; row++) {
+for (let boxCount = 1; boxCount <= 100; boxCount++) {
+
+    //Aggiungiamo i box
+    let boxNormalMarkup = `<div class="box box-${boxCount}">`;
+    const closeNormalMarkup = `</div>`;
+
+
+    container.innerHTML += `${boxNormalMarkup}${boxCount}${closeNormalMarkup}`;
+
 
 }
