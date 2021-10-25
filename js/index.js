b7ab124a34129de0c78501f1af8604cda412d750
dokkalemon <dottori.roberto@gmail..com>
2021-10-25 15:55:09 +0200

    delete and restart

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 const container = document.querySelector('.container');
 
 
+
+
 for (let row = 0; row <= 15; row++) {
 
-    //Aggiungiamo le Righe
-    let rowMarkup = `<div class="row row-${row}">`;
-    container.innerHTML += rowMarkup;
-
-    
 }
