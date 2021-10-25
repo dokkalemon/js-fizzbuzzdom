93a75ff75bd23bd7c2c70e896612ae58867aed4d
dokkalemon <dottori.roberto@gmail..com>
2021-10-25 17:20:31 +0200

    end

--- js/index.js
+++ js/index.js
@@ -6,8 +6,14 @@ const container = document.querySelector('.box-container');
 for (let boxCount = 1; boxCount <= 100; boxCount++) {
 
     //Aggiungiamo i box
-    let boxNormalMarkup = `<div class="box box-${boxCount}">`;
+    if (boxCount % 3 === 0 && boxCount % 5 === 0)  {
+        container.innerHTML += `<div class="box boxFizzBuzz box-${boxCount}">fizzbuzz`;
+    } else if (boxCount % 5 === 0) {
+        container.innerHTML += `<div class="box boxBuzz box-${boxCount}">buzz`;
+    } else if (boxCount % 3 === 0) {
+        container.innerHTML += `<div class="box boxFizz box-${boxCount}">fizz`;
+    } else {
+        container.innerHTML += `<div class="box box-${boxCount}">${boxCount}`;
+    }
 
-
-    container.innerHTML += `${boxNormalMarkup}${boxCount}`;
 }
